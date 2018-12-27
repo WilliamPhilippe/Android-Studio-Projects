d8450188ba2772aa86d18bf6a5f77beef1c0195c
WilliamPhilippe <wplb@ic.ufal.br>
2018-12-27 20:18:43 -0300

    upload

--- Primeiro App/app/src/main/java/com/victoriam/william/primeiroapp/MainActivity.java
+++ Primeiro App/app/src/main/java/com/victoriam/william/primeiroapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.victoriam.william.primeiroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void AlternateText(View view){
+
+        TextView textH = (TextView) findViewById(R.id.textHead);
+        textH.setText("<3");
+
+        TextView textB = (TextView) findViewById(R.id.textBody);
+        textB.setText("Amo você!");
+
+    }
+
 }
